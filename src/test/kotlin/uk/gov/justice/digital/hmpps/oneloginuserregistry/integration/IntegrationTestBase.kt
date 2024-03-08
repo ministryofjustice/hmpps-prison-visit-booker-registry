a7dbb5168282cd190cbed16c5c4cdea7dc151225
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2024-03-08 10:53:06 +0000

    VB-3433 Setup server - One login user registry, some config changes as app..test.yml was incorrectly set up

--- src/test/kotlin/uk/gov/justice/digital/hmpps/oneloginuserregistry/integration/IntegrationTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/oneloginuserregistry/integration/IntegrationTestBase.kt
@@ -5,10 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.DynamicPropertyRegistry
-import org.springframework.test.context.DynamicPropertySource
 import org.springframework.test.web.reactive.server.WebTestClient
-import uk.gov.justice.digital.hmpps.oneloginuserregistry.integration.container.PostgresContainer
 import uk.gov.justice.digital.hmpps.oneloginuserregistry.integration.mock.HmppsAuthExtension
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
@@ -18,24 +15,4 @@ abstract class IntegrationTestBase {
 
   @Autowired
   lateinit var webTestClient: WebTestClient
-
-  companion object {
-
-    private val pgContainer = PostgresContainer.instance
-
-    @JvmStatic
-    @DynamicPropertySource
-    fun properties(registry: DynamicPropertyRegistry) {
-      pgContainer?.run {
-//        registry.add("spring.datasource.url", pgContainer::getJdbcUrl)
-//        registry.add("spring.datasource.username", pgContainer::getUsername)
-//        registry.add("spring.datasource.password", pgContainer::getPassword)
-//        registry.add("spring.datasource.placeholders.database_update_password", pgContainer::getPassword)
-//        registry.add("spring.datasource.placeholders.database_read_only_password", pgContainer::getPassword)
-//        registry.add("spring.flyway.url", pgContainer::getJdbcUrl)
-//        registry.add("spring.flyway.user", pgContainer::getUsername)
-//        registry.add("spring.flyway.password", pgContainer::getPassword)
-      }
-    }
-  }
 }
