32bc380d77f88bc53882919e3cac9c92b2162dae
Aled <101728271+AledWEvans@users.noreply.github.com>
2024-03-08 12:00:15 +0000

    Vb 3433 setup server   one login user registry (#2)
    * updating project name after deployment from template repository
    
    * Updates to names of package and spring boot application
    
    * VB-3433 Setup server - One login user registry
    
    * VB-3433 Setup server - One login user registry, missing postgres-check.sh
    
    * VB-3433 Setup server - One login user registry, missing postgres-check.sh
    
    * VB-3433 Setup server - One login user registry, some config changes as app..test.yml was incorrectly set up
    
    * VB-3433 Setup server - One login user registry, some config changes as app..test.yml was incorrectly set up
    
    * VB-3433 Setup server - One login user registry, some config changes as app..test.yml was incorrectly set up
    
    * VB-3433 Setup server - One login user registry, some config changes as app..test.yml was incorrectly set up
    
    * VB-3433 Setup server - One login user registry, some config changes as app..test.yml was incorrectly set up
    
    * VB-3433 Setup server - One login user registry, some config changes as app..test.yml was incorrectly set up
    
    * VB-3433 Setup server - One login user registry, updated after review

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,65 @@
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.3"
   kotlin("plugin.spring") version "1.9.22"
+  kotlin("plugin.jpa") version "1.9.22"
+  idea
+}
+
+springBoot {
+  mainClass.value("uk.gov.justice.digital.hmpps.oneloginuserregistry.HmppsOneLoginUserRegistryApplication")
 }
 
 configurations {
   testImplementation { exclude(group = "org.junit.vintage") }
 }
 
+repositories {
+  maven { url = uri("https://repo.spring.io/milestone") }
+  mavenCentral()
+}
+
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
+  implementation("org.springframework.boot:spring-boot-starter-security")
+  implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+  implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.1.0")
+
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.16.1")
+  implementation("org.springframework.data:spring-data-commons:3.2.3")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.3.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+  implementation("org.springdoc:springdoc-openapi-starter-common:2.3.0")
+
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  implementation("org.freemarker:freemarker:2.3.32")
+
+  runtimeOnly("org.postgresql:postgresql:42.7.2")
+  runtimeOnly("org.flywaydb:flyway-core")
+
+  annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.20")
+  testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:3.0.1")
+  testImplementation("org.mockito:mockito-inline:5.2.0")
+  testImplementation("org.testcontainers:localstack:1.19.5")
+  testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
+  testImplementation("javax.xml.bind:jaxb-api:2.4.0-b180830.0359")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation("io.jsonwebtoken:jjwt:0.12.5")
+  testImplementation("org.springframework.security:spring-security-test")
+  testImplementation("org.testcontainers:postgresql:1.19.6")
+  testImplementation("org.testcontainers:localstack:1.19.3")
+  testImplementation("com.amazonaws:aws-java-sdk-s3:1.12.668")
+  testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.0")
+}
+
+java {
+  toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
 kotlin {
@@ -21,4 +72,8 @@ tasks {
       jvmTarget = "21"
     }
   }
+}
+
+allOpen {
+  annotation("javax.persistence.Entity")
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/oneloginuserregistry/integration/IntegrationTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/oneloginuserregistry/integration/IntegrationTestBase.kt
@@ -1,16 +1,18 @@
 package uk.gov.justice.digital.hmpps.oneloginuserregistry.integration
 
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.reactive.server.WebTestClient
+import uk.gov.justice.digital.hmpps.oneloginuserregistry.integration.mock.HmppsAuthExtension
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @ActiveProfiles("test")
+@ExtendWith(HmppsAuthExtension::class)
 abstract class IntegrationTestBase {
 
-  @Suppress("SpringJavaInjectionPointsAutowiringInspection")
   @Autowired
   lateinit var webTestClient: WebTestClient
 }
