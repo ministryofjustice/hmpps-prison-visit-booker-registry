d31cd61a78a94c8a72f8d8d7d6e4b03c1b1e667f
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2024-04-16 15:16:48 +0100

    added booker reference object

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prison/visitbooker/registry/dto/BookerReference.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prison/visitbooker/registry/dto/BookerReference.kt
@@ -1,11 +1,13 @@
 package uk.gov.justice.digital.hmpps.prison.visitbooker.registry.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
 
-@Schema(description = "Booker reference value, to be used with all other api call for booker information")
+@Schema(description = "Booker reference Object, to be used with all other api call for booker information")
 data class BookerReference(
-  @Schema(name = "value", required = true)
+  @JsonProperty("value")
+  @Schema(name = "value", description = "This value is the booker reference and should be used to acquire booker information", required = true)
   @field:NotBlank
   val value: String,
 )
