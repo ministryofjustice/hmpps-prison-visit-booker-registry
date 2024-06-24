e1eeb7336d4c122e4dce09e647b66b5976a9f056
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2024-06-24 14:56:44 +0100

    VB-3964 Booker Deactivate Prisoner, after review

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prison/visitbooker/registry/model/repository/PermittedPrisonerRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prison/visitbooker/registry/model/repository/PermittedPrisonerRepository.kt
@@ -1,7 +1,6 @@
 package uk.gov.justice.digital.hmpps.prison.visitbooker.registry.model.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
