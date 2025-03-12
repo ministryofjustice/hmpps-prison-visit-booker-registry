f215c8472c879938f30b8310b23330017a2d68c0
dhirajshettymoj <dhiraj.shetty@digital.justice.gov.uk>
2025-03-12 09:16:17 +0000

    Remove TestRestTemplate as not used.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prison/visitbooker/registry/integration/IntegrationTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prison/visitbooker/registry/integration/IntegrationTestBase.kt
@@ -13,7 +13,6 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
-import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.test.context.ActiveProfiles
@@ -71,9 +70,6 @@ abstract class IntegrationTestBase {
       visitSchedulerMockServer.stop()
     }
   }
-
-  @Autowired
-  private lateinit var testRestTemplate: TestRestTemplate
 
   @Autowired
   lateinit var webTestClient: WebTestClient
