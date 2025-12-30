e272b559881c45ca268c16af56d400a7bd212db2
dhirajshettymoj <dhiraj.shetty@digital.justice.gov.uk>
2025-12-30 12:52:38 +0000

    Fix for VB-6825.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prison/visitbooker/registry/integration/GetCountVisitorRequestsForPrisonTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prison/visitbooker/registry/integration/GetCountVisitorRequestsForPrisonTest.kt
@@ -68,6 +68,53 @@ class GetCountVisitorRequestsForPrisonTest : IntegrationTestBase() {
   }
 
   @Test
+  fun `when prisoner has booker in multiple prisons then count is returned only for prison where the requesting booker has registered prisoner`() {
+    // Given
+    val otherPrisonCode = "XYZ"
+    val prisonerId = "AA123456"
+
+    // prisoner AA123456 has 2 bookers
+    // booker1 has registered prisoner against HEI
+    val booker1 = createBooker("abc-def-ghi", "test@test.com")
+    val prisoner = createPrisoner(booker1, prisonerId, prisonCode = prisonCode)
+
+    // booker2 has registered same prisoner against XYZ
+    val booker2 = createBooker("ccc-def-ghi", "test1@example.com")
+    createPrisoner(booker2, prisonerId, prisonCode = otherPrisonCode)
+
+    createVisitorRequest(
+      booker1.reference,
+      prisoner.prisonerId,
+      AddVisitorToBookerPrisonerRequestDto("firstName1", "lastName1", LocalDate.now().minusYears(21)),
+      status = VisitorRequestsStatus.REQUESTED,
+    )
+    createVisitorRequest(
+      booker1.reference,
+      prisoner.prisonerId,
+      AddVisitorToBookerPrisonerRequestDto("firstName2", "lastName2", LocalDate.now().minusYears(21)),
+      status = VisitorRequestsStatus.APPROVED,
+    )
+
+    // When
+    // get count by HEI should return 1
+    var responseSpec = getCountVisitorRequestsByPrisonCode(webTestClient, prisonCode, orchestrationServiceRoleHttpHeaders)
+
+    // Then
+    var returnResult = responseSpec.expectStatus().isOk.expectBody()
+    var responseDto = getResults(returnResult)
+    Assertions.assertThat(responseDto.count).isEqualTo(1)
+
+    // When
+    // get count by XYZ should return 0
+    responseSpec = getCountVisitorRequestsByPrisonCode(webTestClient, otherPrisonCode, orchestrationServiceRoleHttpHeaders)
+
+    // Then
+    returnResult = responseSpec.expectStatus().isOk.expectBody()
+    responseDto = getResults(returnResult)
+    Assertions.assertThat(responseDto.count).isEqualTo(0)
+  }
+
+  @Test
   fun `access forbidden when no role`() {
     // When
     val responseSpec = getCountVisitorRequestsByPrisonCode(webTestClient, prisonCode, setAuthorisation(roles = listOf()))
