64d83db2e2bb90d81ff16ff42a78d4711314395b
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2026-03-10 12:19:17 +0000

    VB-6473 - Add new withRestrictions flag (default to false)
    We never need restrictions here, defaulting to false. Also removing
    the withAddress flag as it's been removed from the API.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prison/visitbooker/registry/integration/mock/PrisonerContactRegistryMockServer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prison/visitbooker/registry/integration/mock/PrisonerContactRegistryMockServer.kt
@@ -30,33 +30,11 @@ class PrisonerContactRegistryMockServer : WireMockServer(8093) {
     )
   }
 
-  fun stubGetPrisonerApprovedContacts(
-    prisonerId: String,
-    contactsList: List<PrisonerContactDto>?,
-    httpStatus: HttpStatus = HttpStatus.NOT_FOUND,
-  ) {
-    val responseBuilder = createJsonResponseBuilder()
-
-    stubFor(
-      get("/v2/prisoners/$prisonerId/contacts/social/approved?${getContactsQueryParams()}")
-        .willReturn(
-          if (contactsList == null) {
-            responseBuilder
-              .withStatus(httpStatus.value())
-          } else {
-            responseBuilder
-              .withStatus(HttpStatus.OK.value())
-              .withBody(getJsonString(contactsList))
-          },
-        ),
-    )
-  }
-
   private fun getContactsQueryParams(): String {
     val queryParams = kotlin.collections.ArrayList<String>()
 
     queryParams.add("hasDateOfBirth=false")
-    queryParams.add("withAddress=false")
+    queryParams.add("withRestrictions=false")
 
     return queryParams.joinToString("&")
   }
