0aebfb104fcb1210db5d9b9cde0bd535b005d3f2
Aaron Davies <168211160+adaviesMOJ@users.noreply.github.com>
2026-04-17 14:52:26 +0100

    VB-6616 - Remove incorrect domain event processing (#313)
    The prisoner-contact-updated event seems to only relate to the
    relationship itself, not the contact details.
    
    We need the contact-updated event. Removing this one and will
    add the correct one in the next PR.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prison/visitbooker/registry/enums/DomainEventTypes.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prison/visitbooker/registry/enums/DomainEventTypes.kt
@@ -2,5 +2,4 @@ package uk.gov.justice.digital.hmpps.prison.visitbooker.registry.enums
 
 enum class DomainEventTypes(val value: String) {
   PRISONER_CONTACT_CREATED_EVENT("contacts-api.prisoner-contact.created"),
-  PRISONER_CONTACT_UPDATED_EVENT("contacts-api.prisoner-contact.updated"),
 }
